f523bab729b006b4d2d39d07082f0fe0882563a9
Leopold Wieser <leopoldwieser1@gmail.com>
2023-03-23 17:36:42 +0100

    initial commit

---
+++ frontend.Dockerfile
@@ -0,0 +1,20 @@
+FROM node:18
+
+# Create a directory for the app and copy the package.json file
+RUN mkdir -p /app
+
+COPY package.json /app
+
+# Install dependencies
+RUN cd /app && npm install
+#RUN npm install -g serve
+
+
+# Copy the rest of the app's code
+COPY . /app
+WORKDIR /app
+
+
+# Expose port 3000 and run the app
+EXPOSE 3000
+CMD ["npm", "run", "dev"]
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      "/api": {
+        target: 'http://127.0.0.1:5000',
+        // target: 'http://backend:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
+  }
+})
